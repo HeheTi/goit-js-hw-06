09d57eb772e634dd90e394aedfaa3ed70d764959
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-08 01:31:13 +0200

    done 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const makeitemsArray = (array) => {
+  return array.map((element) => {
+    const item = document.createElement("li");
+    item.textContent = `${element}`;
+    item.classList.add("item");
+    return item;
+  });
+};
+
+const items = makeitemsArray(ingredients);
+
+listEl.append(...items);
