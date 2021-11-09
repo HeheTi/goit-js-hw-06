f5d3f99e739a7d21f516f4612cc9929a3298c0c1
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-09 19:11:53 +0200

    done 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+
+const changeBgColor = function () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorRef.textContent = color;
+};
+
+buttonRef.addEventListener("click", changeBgColor);
